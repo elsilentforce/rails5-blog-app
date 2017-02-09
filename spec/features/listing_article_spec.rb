0da70a07ef08733956c3c55891e90ef5ec8cca44
Patricio Tapia <native.devil@gmail.com>
2017-02-09 11:51:40 -0300

    REFACT: Index view and Controller

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
-RSpec.feature "Creating Articles" do
+RSpec.feature "Listing Articles" do
 
   before do
-    @article1 = Article.new(title: "First test article", body: "Body of first test article")
-    @article2 = Article.new(title: "Second test article", body: "Body of second test article")
+    @article1 = Article.create(title: "First test article", body: "Body of first test article")
+    @article2 = Article.create(title: "Second test article", body: "Body of second test article")
   end
 
   scenario "A user list all Articles" do
     visit "/"
     expect(page).to have_content(@article1.title)
-    expect(page).to have_content(@article2.title)
     expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
     expect(page).to have_content(@article2.body)
 
     expect(page).to have_link(@article1.title)
