2c27607cb8479ba9da78a2ad972aef1e5c7a2730
Patricio Tapia <native.devil@gmail.com>
2017-02-12 12:34:42 -0300

    REFACT: Expect flash message on article request spec

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -18,6 +18,8 @@ RSpec.describe "Articles", type: :request do
       before { get '/articles/xxx' }
       it 'handles non-existing article' do
         expect (response.status).to eq 302
+        flash_message = "The article you are looking for could not be found"
+        expect(flash[:alert]).to eq flash_message
       end
     end
 
