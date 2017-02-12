93c7935d8c1b19b689e7dcfccc360dd779534798
Patricio Tapia <native.devil@gmail.com>
2017-02-12 12:36:23 -0300

    ADD: Override resource_not_found method on articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,8 +23,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  protected
+    def resource_not_found
+      message = "The article you are looking for could not be found"
+      flash[:alert] = message
+      redirect_to root_path
+    end
+
   private
-  def article_params
-    params.require(:article).permit(:title, :body)
-  end
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
