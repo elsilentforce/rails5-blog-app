4db47caef959b7f48e8a76b21e1bcdb85f6490b7
Patricio Tapia <native.devil@gmail.com>
2017-02-12 12:32:42 -0300

    ADD: rescue_from and abstract methond on application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+  protected
+    def record_not_found
+    end
 end
