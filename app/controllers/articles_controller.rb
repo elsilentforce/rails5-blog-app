d7b4eacd7e034c59673b6bb524d86773dad0c830
Patricio Tapia <native.devil@gmail.com>
2017-02-12 23:23:30 -0300

    ADD: update action on articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
+  end
+
   protected
     def resource_not_found
       message = "The article you are looking for could not be found"
