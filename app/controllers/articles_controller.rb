0c9666af48f7a497ffbde4e3000b53fe540e0ea4
Patricio Tapia <native.devil@gmail.com>
2017-02-16 15:22:24 -0300

    REFACT: set_article method on Articles Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show,:edit,:update,:destroy]
+
   def index
     @articles = Article.all
   end
@@ -20,15 +22,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
       redirect_to @article
@@ -39,7 +38,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = "Article has been deleted"
       redirect_to articles_path
@@ -54,6 +52,10 @@ class ArticlesController < ApplicationController
     end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :body)
     end
