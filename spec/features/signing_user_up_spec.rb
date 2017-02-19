527ea9ea29b36862a0dd67b1005d028186556af9
Patricio Tapia <native.devil@gmail.com>
2017-02-19 14:39:28 -0300

    ADD: User signup specs

---
+++ spec/features/signing_user_up_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.feature "Users Signup" do
+  scenario "with valid credentials" do
+    visit "/"
+    click_link "Sign Up"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("You have signed up successfully.")
+
+  end
+
+  scenario "with invalid credentials" do
+    visit "/"
+    click_link "Sign Up"
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_button "Sign up"
+  end
+end
