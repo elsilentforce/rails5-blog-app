87bb0bbbe45d9f9859e1d856f207d729434aeaae
Patricio Tapia <native.devil@gmail.com>
2017-03-17 14:50:06 -0300

    REFACT: comments_controller#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ def create
     @comment = @article.comments.build(comment_params)
     @comment.user = current_user
     if @comment.save
+      ActionCable.server.broadcast "comments", render(partial: "comments/comment", object: @comment)
       flash[:notice] = "Comment has been created"
     else
       flash[:alert] = "Comment has not been created"
