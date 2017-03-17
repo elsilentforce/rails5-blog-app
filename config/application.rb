dcd43de5ba24a8a8e50177f851bd900bbf59be8c
Patricio Tapia <native.devil@gmail.com>
2017-03-17 14:44:37 -0300

    ADD: config.action_cable.mount_path on config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,4 +22,7 @@ module BlogApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+  config.action_cable.mount_path = '/cable'
+  
 end
